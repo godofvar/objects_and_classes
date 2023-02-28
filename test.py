35e6c30be25ab177bc2daf3255add39608586ec8
Igor Grachev <pit1507@rambler.ru>
2023-02-28 22:52:26 +0300

    minor edits

--- test.py
+++ test.py
@@ -43,11 +43,11 @@ class Reviewer(Mentor):
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+reviewer = Reviewer('Some', 'Buddy')
+reviewer.courses_attached += ['Python']
+
+reviewer.rate_hw(best_student, 'Python', 10)
+reviewer.rate_hw(best_student, 'Python', 10)
+reviewer.rate_hw(best_student, 'Python', 10)
  
 print(best_student.grades)
