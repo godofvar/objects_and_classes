a6041644eade657482abef567631abb9e5f0afe9
Igor Grachev <pit1507@rambler.ru>
2023-02-28 22:09:36 +0300

    creating Lecturer and Reviewer classes

--- test.py
+++ test.py
@@ -25,6 +25,15 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
