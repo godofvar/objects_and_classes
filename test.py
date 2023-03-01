382ca2a48b24b17a307aebf4ed231ff68e9d43ae
Igor Grachev <pit1507@rambler.ru>
2023-03-01 23:36:06 +0300

    implementation of the magic method __str__ for all classes; implementation of comparison between lecturers by average grade for lectures and students by average grade for homework

--- test.py
+++ test.py
@@ -7,6 +7,29 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def __str__(self):
+        all_ratings = sum(self.grades.values(), start=[])
+        if all_ratings == []:
+            return f'Имя: {self.name}' + '\n' + f'Фамилия: {self.surname}' + '\n'\
+                + f'Без оценок за дз'  + '\n'\
+                    + f'Курсы в процессе изучения: {list(self.grades.keys())}' + '\n'\
+                        +f'Завершенные курсы: {self.finished_courses}'
+        else:
+            return f'Имя: {self.name}' + '\n' + f'Фамилия: {self.surname}' + '\n'\
+                + f'Средняя оценка за дз: {round(sum(all_ratings) / len(all_ratings), 2)}'  + '\n'\
+                    + f'Курсы в процессе изучения: {list(self.grades.keys())}' + '\n'\
+                        +f'Завершенные курсы: {self.finished_courses}'
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+           print('Not a Student!')
+           return
+        all_ratings_self, all_ratings_other = sum(self.grades.values(), start=[]), sum(other.grades.values(), start=[])
+        return round(sum(all_ratings_self) / len(all_ratings_self), 2) < round(sum(all_ratings_other) / len(all_ratings_other), 2)
+
+    def added_courses(self, course_name):
+        self.finished_courses.append(course_name)   
+
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
             if course in lecturer.grades:
@@ -22,14 +45,32 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+    def __str__(self):
+        return f'Имя: {self.name}' + '\n' + f'Фамилия: {self.surname}'
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __str__(self):
+        all_ratings = sum(self.grades.values(), start=[])
+        return f'Имя: {self.name}' + '\n' + f'Фамилия: {self.surname}' + '\n'\
+            + f'Средняя оценка за лекции: {round(sum(all_ratings) / len(all_ratings), 2)}'   
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+           print('Not a Lecturer!')
+           return
+        all_ratings_self, all_ratings_other = sum(self.grades.values(), start=[]), sum(other.grades.values(), start=[])
+        return round(sum(all_ratings_self) / len(all_ratings_self), 2) < round(sum(all_ratings_other) / len(all_ratings_other), 2)
+
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+
+    def __str__(self):
+        return f'Имя: {self.name}' + '\n' + f'Фамилия: {self.surname}'
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -39,7 +80,7 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
